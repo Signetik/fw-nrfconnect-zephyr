2b84329eae335f075f983951684250645c363142
Steve Poulsen <spoulsen@signetik.com>
2020-03-06 17:06:17 -0600

    Fixup regulators build.

--- drivers/regulator/regulator_generic.c
+++ drivers/regulator/regulator_generic.c
@@ -23,8 +23,8 @@
 
 #include "regulator.h"
 
+#if defined(DT_SIGNETIK_REGULATORS_LSENSE_ADDON_GPIOS_CONTROLLER)
 DEVICE_DECLARE(lsensor_reg);
-DEVICE_DECLARE(accel_reg);
 
 static struct regulator_data lsensor_reg_data;
 static struct regulator_config lsensor_reg_config = {
@@ -37,21 +37,26 @@ static struct regulator_api lsensor_reg_api = {
     .na = NULL
 };
 
+DEVICE_AND_API_INIT(lsensor_reg, "lsensor_reg", regulator_init, &lsensor_reg_data,
+		    &lsensor_reg_config, PRE_KERNEL_2, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                    &lsensor_reg_api);
+#endif
+
+#if defined(DT_SIGNETIK_REGULATORS_ACCEL_ADDON_GPIOS_CONTROLLER)
+DEVICE_DECLARE(accel_reg);
+
 static struct regulator_data accel_reg_data;
 static struct regulator_config accel_reg_config = {
     .en_cont = DT_SIGNETIK_REGULATORS_ACCEL_ADDON_GPIOS_CONTROLLER,
     .en_pin = DT_SIGNETIK_REGULATORS_ACCEL_ADDON_GPIOS_PIN,
-    .flags = DT_SIGNETIK_REGULATORS_LSENSE_ADDON_GPIOS_FLAGS,
+    .flags = DT_SIGNETIK_REGULATORS_ACCEL_ADDON_GPIOS_FLAGS,
     .use_init = 1,
 };
 static struct regulator_api accel_reg_api = {
     .na = NULL
 };
 
-DEVICE_AND_API_INIT(lsensor_reg, "lsensor_reg", regulator_init, &lsensor_reg_data,
-		    &lsensor_reg_config, PRE_KERNEL_2, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
-                    &lsensor_reg_api);
-
 DEVICE_AND_API_INIT(accel_reg, "accel_reg", regulator_init, &accel_reg_data,
 		    &accel_reg_config, PRE_KERNEL_2, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
                     &accel_reg_api);
+#endif
